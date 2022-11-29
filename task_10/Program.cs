cad8d5dd4b165272cb99cb2d64a511ad9843fcf3
timurich80 <timurich80@gmail.com>
2022-11-29 12:04:02 +0300

    first commit

---
+++ task_10/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+
+Console.Clear();
+System.Console.WriteLine("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+int result = (a/10) % 10;
+System.Console.WriteLine(result);
---
+++ task_13/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+
+
+Console.Clear();
+System.Console.WriteLine("Введите трехзначное число: ");
+string a = Convert.ToString(Console.ReadLine());
+if(a.Length > 2)
+{
+    System.Console.WriteLine($"{a} -> {a[2]}");
+}
+else Console.WriteLine($" {a} -> третьей цифры нет");
